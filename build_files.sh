c184172a76de4000d2ac78d1a62f84274431d370
anandachanta19 <anandachanta19@gmail.com>
2024-11-18 13:18:03 +0530

    vercel configuration

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Download and install pip if it’s not available
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install -r requirements.txt
-python3.12 manage.py collectstatic
+python3.9 get-pip.py
+
+# Install dependencies from requirements.txt
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
