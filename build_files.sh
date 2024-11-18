9413e22594b20a620be196d642d489816d456fd8
anandachanta19 <anandachanta19@gmail.com>
2024-11-18 14:43:39 +0530

    vercel configuration

--- build_files.sh
+++ build_files.sh
@@ -6,4 +6,4 @@ source venv/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py
