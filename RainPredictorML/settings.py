e5b43eaeefc99ece8d24011aa17cda7f827ea22d
anandachanta19 <anandachanta19@gmail.com>
2024-11-18 14:11:11 +0530

    vercel configuration

--- RainPredictorML/settings.py
+++ RainPredictorML/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'RainPredictorML.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'EvZqTueBvmuLDhrJknSnXfmQwkGarqMB',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
